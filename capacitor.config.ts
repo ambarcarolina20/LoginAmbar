1c09e82fb14d3099be2bf64526b9cb81532d17a8
Ambar Colmenarez <102635271+ambarcarolina20@users.noreply.github.com>
2023-09-30 23:50:40 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto_vehiculo_front',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
